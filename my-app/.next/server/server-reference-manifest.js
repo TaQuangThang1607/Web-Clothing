b917f44b41608b8e24348d1773115d1b46b2e578
TaQuangThang <yuuwhisky@gmail.com>
2025-05-26 10:11:53 +0700

    update .next

--- my-app/.next/server/server-reference-manifest.js
+++ my-app/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"Q//+ZwAI3foaA9HJl5MEyXJN2boM6lMCASZGIAelOrE=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
